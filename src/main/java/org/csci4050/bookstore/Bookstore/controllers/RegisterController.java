d40d0f8bc7b07fc7133b8d9ccdb47919a9de5409
John Michael Kovachi <johnm.kovachi@gmail.com>
2018-11-14 01:31:50 -0500

    Add example json for registerCustomer

--- src/main/java/org/csci4050/bookstore/Bookstore/controllers/RegisterController.java
+++ src/main/java/org/csci4050/bookstore/Bookstore/controllers/RegisterController.java
@@ -23,9 +23,27 @@ public class RegisterController {
 
     @RequestMapping(value = "customer", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
+    /**
+     * Example JSON body for posting to registerCustomer
+     * {
+     * 	"firstName" : "John",
+     * 	"minit" : "M",
+     * 	"lastName" : "Kovachi",
+     * 	"address" : "3735 Tulip Tree Rd",
+     * 	"birthDate" : "09-18-1996",
+     * 	"verified" : "false",
+     * 	"newsletter" : "true",
+     * 	"username" : "jm",
+     * 	"password" : "newPass",
+     * 	"email" : "gmail.com",
+     * 	"role" : "ROLE_CUSTOMER",
+     * 	"imageUrl" : "url"
+     * }
+     */
     public String registerCustomer(@RequestBody final String jsonCustomer) throws Exception {
         final Customer customer = gson.fromJson(jsonCustomer, Customer.class);
         customerService.registerCustomer(customer);
         return gson.toJson(customer);
     }
+
 }
