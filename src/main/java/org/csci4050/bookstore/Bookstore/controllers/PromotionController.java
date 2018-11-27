9ee87c3792c7546f8208d46b73e50df6983e0800
John Michael Kovachi <johnm.kovachi@gmail.com>
2018-11-27 03:04:53 -0500

    Add apply promotion

--- src/main/java/org/csci4050/bookstore/Bookstore/controllers/PromotionController.java
+++ src/main/java/org/csci4050/bookstore/Bookstore/controllers/PromotionController.java
@@ -1,15 +1,23 @@
 package org.csci4050.bookstore.Bookstore.controllers;
 
+import lombok.Builder;
+import lombok.Data;
+import org.csci4050.bookstore.Bookstore.exceptions.ValidationException;
 import org.csci4050.bookstore.Bookstore.model.Promotion;
+import org.csci4050.bookstore.Bookstore.service.CartService;
 import org.csci4050.bookstore.Bookstore.service.PromotionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/promotion")
@@ -17,6 +25,9 @@ public class PromotionController {
 
     @Autowired
     private PromotionService promotionService;
+
+    @Autowired
+    private CartService cartService;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public ResponseEntity<Object> addPromotion(@RequestBody final Promotion promotion) {
@@ -28,5 +39,23 @@ public class PromotionController {
     public ResponseEntity<Object> editPromotion(@RequestBody final Promotion promotion) {
         promotionService.updatePromotion(promotion);
         return new ResponseEntity<>("", new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/apply/{promoCode}", method = RequestMethod.POST)
+    public ResponseEntity<Object> applyPromotion(@PathVariable final int promoCode, final Principal principal) throws ValidationException {
+        final String username = principal.getName();
+        final Optional<Promotion> promotionOptional = promotionService.getPromotionWithPromoCode(promoCode);
+        if (!promotionOptional.isPresent()) {
+            throw new ValidationException("Promotion with promo code <%s> could not be applied", Integer.toString(promoCode));
+        }
+        final Promotion promotion = promotionOptional.get();
+        promotionService.updateCartWithPromotion(promotion, username);
+        return new ResponseEntity<>("", new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @Data
+    @Builder
+    public static class Promo {
+        private int promoCode;
     }
 }
--- src/main/java/org/csci4050/bookstore/Bookstore/service/PromotionService.java
+++ src/main/java/org/csci4050/bookstore/Bookstore/service/PromotionService.java
@@ -1,22 +1,32 @@
 package org.csci4050.bookstore.Bookstore.service;
 
 import org.csci4050.bookstore.Bookstore.dao.PromotionDao;
+import org.csci4050.bookstore.Bookstore.exceptions.ValidationException;
+import org.csci4050.bookstore.Bookstore.model.CartItem;
 import org.csci4050.bookstore.Bookstore.model.Promotion;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.Optional;
 
 public class PromotionService {
 
     private PromotionDao promotionDao;
 
+    private CartService cartService;
+
     @Autowired
-    public PromotionService(final PromotionDao promotionDao) {
+    public PromotionService(final PromotionDao promotionDao, final CartService cartService) {
         this.promotionDao = promotionDao;
+        this.cartService = cartService;
     }
 
     public Optional<Promotion> getPromotion(final int promoId) {
         return this.promotionDao.getPromotion(promoId);
+    }
+
+    public Optional<Promotion> getPromotionWithPromoCode(final int promoCode) {
+        return this.promotionDao.getPromotionWithPromoCode(promoCode);
     }
 
     public void createPromotion(final Promotion promotion) {
@@ -25,5 +35,14 @@ public class PromotionService {
 
     public void updatePromotion(final Promotion promotion) {
         promotionDao.updatPromotion(promotion);
+    }
+
+    public void updateCartWithPromotion(final Promotion promotion, final String username) throws ValidationException {
+        final List<CartItem> cartItems = cartService.getCartForCustomer(username);
+        for (final CartItem item : cartItems) {
+            final Double promoPrice = item.getOriginalPrice()*((100-promotion.getPercentOff())/100);
+            item.setFinalPrice(promoPrice);
+            cartService.updateCartItem(item);
+        }
     }
 }
