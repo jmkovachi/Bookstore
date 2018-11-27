adf693689e260d4c350e6413dd5031f437676a7d
JP Reinold <rhinojon@gmail.com>
2018-11-26 23:45:58 -0500

    Add order confirmation jsp

--- src/main/java/org/csci4050/bookstore/Bookstore/controllers/OrderConfirmationController.java
+++ src/main/java/org/csci4050/bookstore/Bookstore/controllers/OrderConfirmationController.java
@@ -1,9 +1,10 @@
 package org.csci4050.bookstore.Bookstore.controllers;
 
+import lombok.Builder;
+import lombok.Data;
 import org.csci4050.bookstore.Bookstore.exceptions.ValidationException;
-import org.csci4050.bookstore.Bookstore.model.Order;
-import org.csci4050.bookstore.Bookstore.model.OrderItem;
-import org.csci4050.bookstore.Bookstore.model.ShippingAddress;
+import org.csci4050.bookstore.Bookstore.model.*;
+import org.csci4050.bookstore.Bookstore.service.BookService;
 import org.csci4050.bookstore.Bookstore.service.OrderService;
 import org.csci4050.bookstore.Bookstore.service.ShippingAddressService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class OrderConfirmationController {
@@ -24,6 +26,9 @@ public class OrderConfirmationController {
 
     @Autowired
     private ShippingAddressService shippingAddressService;
+
+    @Autowired
+    private BookService bookService;
 
     @RequestMapping(value = "/order/confirm/{orderId}", method = RequestMethod.GET)
     public ModelAndView orderConfirmation(@PathVariable final int orderId) throws ValidationException {
@@ -37,14 +42,35 @@ public class OrderConfirmationController {
             shippingAddress = shippingAddressService.getShippingAddress(order.getAddressId()).get();
         }
 
-        final OrderController.CheckoutInfo checkoutInfo = OrderController.CheckoutInfo.builder()
+        final OrderConfirmationInfo orderConfirmationInfo = OrderConfirmationInfo.builder()
                 .order(order)
-                .orderItems(orderItems)
-                .shippingAddress(shippingAddress)
+                .orderItemWithBooks(orderItems.stream().map(this::transformOrderItemToOrderItemWithBook).collect(Collectors.toList()))
                 .build();
 
-        return new ModelAndView("views/confirm-order", "confirm", checkoutInfo);
+        return new ModelAndView("views/confirm-order", "confirm", orderConfirmationInfo);
     }
 
+    @Data
+    @Builder
+    public static class OrderItemWithBook {
+        private OrderItem orderItem;
+        private Book book;
+    }
+
+    @Data
+    @Builder
+    public static class OrderConfirmationInfo {
+        private Order order;
+        private List<OrderItemWithBook> orderItemWithBooks;
+        private ShippingAddress shippingAddress;
+    }
+
+    private OrderItemWithBook transformOrderItemToOrderItemWithBook(final OrderItem orderItem) {
+        final Book book = bookService.getBook(orderItem.getIsbn()).get();
+        return OrderItemWithBook.builder()
+                .book(book)
+                .orderItem(orderItem)
+                .build();
+    }
 
 }
--- src/main/webapp/WEB-INF/views/confirm-order.jsp
+++ src/main/webapp/WEB-INF/views/confirm-order.jsp
@@ -1,4 +1,20 @@
 <!DOCTYPE html>
-<html>
-    <h1> Confirmation sent </h1>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="tags" tagdir="/WEB-INF/tags/"%>
+<html lang="en">
+    <head>
+    </head>
+    <body>
+        <div class="col-md-12 center-block text-center">
+            <p class="card-text">Order confirmation<br /> <br />
+                ${confirm.order.username}, your order has been placed! <br />
+                Thank you for shopping at Bookstore.com!
+
+            </p>
+        </div>
+
+
+
+    </body>
 </html>
